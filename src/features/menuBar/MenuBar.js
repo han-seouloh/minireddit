abe90629ffea2d1fa56da9c9dd11bf10e37d9e8a
HanSeoulOh <112726708+han-seouloh@users.noreply.github.com>
2022-12-25 20:36:59 -0600

    Refactor code for better presentability

--- src/features/menuBar/MenuBar.js
+++ src/features/menuBar/MenuBar.js
@@ -1,6 +1,7 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { selectMenu, toggleMenu } from './menuBarSlice';
 import { Subreddit } from '../subreddit/Subreddit';
+import { About } from '../about/About';
 
 //Import icon
 import { RiArrowLeftSLine } from 'react-icons/ri';
@@ -33,6 +34,9 @@ export const MenuBar = () => {
     <div id='menubar' className={`${styles.menuBar} ${menu ? styles['returnMenuBar'] : styles['hideMenuBar']}`}>
       <div className={styles['hide-menu']} >
         <RiArrowLeftSLine className={styles.arrowIcon} onClick={handleMenuClick} />
+      </div>
+      <div className={styles['about-menu']}>
+        <About />
       </div>
       <div className={styles['subreddit-menu']} >
         {subreddits.map(sub => <Subreddit subname={sub} />)}
