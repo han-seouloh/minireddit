62458dcf5f5f3a34cd897206e3b6d6b8014f2451
HanSeoulOh <112726708+han-seouloh@users.noreply.github.com>
2022-12-28 21:59:25 -0600

    Add posts slice to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import menuBarReducer from "../features/menuBar/menuBarSlice";
 import subredditReducer from "./subredditSlice";
+import postsReducer from "./postsSlice";
 
 export default configureStore({
   reducer: {
     menu: menuBarReducer,
-    subreddit: subredditReducer
+    subreddit: subredditReducer,
+    posts: postsReducer
   }
 });
