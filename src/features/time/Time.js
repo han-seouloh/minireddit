8dfd39f5cee11512c408e780a9055cbfc3817158
HanSeoulOh <112726708+han-seouloh@users.noreply.github.com>
2023-01-01 14:07:50 -0600

    Finish time selection component

---
+++ src/features/time/Time.js
@@ -0,0 +1,20 @@
+import { setTime } from '../../store/postsSlice';
+import { useDispatch } from 'react-redux';
+import styles from './Time.module.css';
+
+export const Time = () => {
+  const dispatch = useDispatch();
+  const handleChange = ({target}) => {
+    dispatch(setTime(target.value));
+  }
+
+  return (
+    <select name='time' className={styles['dropdown']} onChange={handleChange}>
+      <option value='now'>Now</option>
+      <option value='day'>Day</option>
+      <option value='month'>Month</option>
+      <option value='year'>Year</option>
+      <option value='all'>All Time</option>
+    </select>
+  );
+}
