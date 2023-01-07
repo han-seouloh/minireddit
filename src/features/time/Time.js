9b59bdd191c196fc74942295f9571f9a4f36accc
HanSeoulOh <112726708+han-seouloh@users.noreply.github.com>
2023-01-06 19:30:23 -0600

    Update and cleanup time component

--- src/features/time/Time.js
+++ src/features/time/Time.js
@@ -10,7 +10,6 @@ export const Time = () => {
 
   return (
     <select name='time' className={styles['dropdown']} onChange={handleChange}>
-      <option value='hour'>Now</option>
       <option value='day'>Day</option>
       <option value='month'>Month</option>
       <option value='year'>Year</option>
