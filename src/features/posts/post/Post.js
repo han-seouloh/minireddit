0b2f1bbe68a6bfd51c7a669e05466e63f7d06bb7
HanSeoulOh <112726708+han-seouloh@users.noreply.github.com>
2023-01-06 18:54:37 -0600

    Add comments component for rendering

--- src/features/posts/post/Post.js
+++ src/features/posts/post/Post.js
@@ -4,6 +4,11 @@ import { formatNum } from '../../../utils/formatNum';
 import { Media } from '../media/Media';
 import moment from 'moment';
 import styles from './Post.module.css'
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { getComments } from '../../../api/reddit';
+import { removeComments } from '../../../store/commentsSlice';
+import { Comments } from '../../comments/Comments';
 
 moment.updateLocale('en', {
   relativeTime : {
@@ -13,6 +18,20 @@ moment.updateLocale('en', {
 });
 
 export const Post = ({data}) => {
+  const [commentVisibility, setCommentVisibility] = useState(false);
+  const {subreddit, id:postId} = data;
+  const dispatch = useDispatch();
+
+  const showComments = () => {
+    if (commentVisibility) {
+      setCommentVisibility(false);
+      dispatch(removeComments({id:postId}));
+    } else {
+      setCommentVisibility(true);
+      dispatch(getComments({subreddit, postId}));
+    };
+    
+  }
 
   return (
     <div className={styles['post-container']}>
@@ -22,13 +41,19 @@ export const Post = ({data}) => {
           <Media data={data} />
         </div>
         <p className={styles['text-display']}>{data.selftext}</p>
+        {
+          ((data.subreddit==='worldnews')
+          || (data.subreddit==='todayilearned')
+          || (data.subreddit==='nottheonion'))
+          && <a href={data.url} className={styles['text-display']}>{data.url}</a>
+        }
       </div>
       <div className={styles['data-container']}>
         <p className={styles['subreddit-display']}>Posted by <span>{data.author}</span> at <span>{data['subreddit_name_prefixed']}</span></p>
         <div className={styles['stats-container']}>
           <p className={styles['time-display']}>{moment.unix(data.created).fromNow()}</p>
           <div className={styles['comments-container']}>
-            <BiCommentDetail className={styles['comments-icon']}/>
+            <BiCommentDetail className={styles['comments-icon']} onClick={showComments} />
             <p className={styles['comments-count']}>{formatNum(data.num_comments)}</p>
           </div>
         </div>
@@ -38,6 +63,10 @@ export const Post = ({data}) => {
         <p className={styles['upvote-count']}>{formatNum(data.ups)}</p>
         <TiArrowDownOutline />
       </div>
+      {commentVisibility
+        ? <Comments postId={postId} />
+        : null 
+      }
     </div>
   )
 };
